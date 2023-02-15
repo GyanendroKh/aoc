c3cc5f1d77a0192c1d1c0578922a067a4f38159a
Gyanendro Kh <16163526+GyanendroKh@users.noreply.github.com>
2023-02-15 19:46:27 +0530

    Day 06 (part 2)

--- 06/main.ts
+++ 06/main.ts
@@ -2,9 +2,9 @@ const decoder = new TextDecoder('utf-8');
 const inputs = decoder.decode(Deno.readFileSync('input.txt')).trim();
 
 for (let i = 4; i < inputs.length; i++) {
-  const scope = inputs.substring(i - 4, i);
+  const scope = inputs.substring(i - 14, i);
 
-  if (new Set(scope.split('')).size == 4) {
+  if (new Set(scope.split('')).size == 14) {
     console.log(scope, i);
     break;
   }
